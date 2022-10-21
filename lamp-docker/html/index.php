acdc4465d69ee25ddbe6b883359b435e6d224aba
da.matsumoto <the-carp-play-the-harp@ezweb.ne.jp>
2022-10-21 10:27:51 +0900

    first commit

---
+++ lamp-docker/html/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>動作確認</title>
+</head>
+<body>
+  <h1><?php 
+  print("DockerでPHP環境構築しました");
+  ?></h1>
+</body>
+</html>
